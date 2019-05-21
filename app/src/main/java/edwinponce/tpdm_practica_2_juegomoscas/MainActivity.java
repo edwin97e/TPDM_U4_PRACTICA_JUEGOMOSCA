f1501e806beeb6690de6fc742eae2c0c78782fc9
edwin97e <edwin97e@gmail.com>
2019-05-20 21:29:47 -0600

    first commit

---
+++ app/src/main/java/edwinponce/tpdm_practica_2_juegomoscas/MainActivity.java
@@ -0,0 +1,13 @@
+package edwinponce.tpdm_practica_2_juegomoscas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new Lienzo(this));
+    }
+}
